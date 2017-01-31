0b5e41a8bb4dd8033d0c0f1be4a14b6ff6bb5ee5
Alex Rupérez <alejandro.ruperez@intelygenz.com>
2017-01-31 12:20:26 +0100

    Initial Commit

---
+++ IGZLocation/IGZLocation.h
@@ -0,0 +1,19 @@
+//
+//  IGZLocation.h
+//  IGZLocation
+//
+//  Created by Alejandro Ruperez Hernando on 31/1/17.
+//  Copyright © 2017 Intelygenz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IGZLocation.
+FOUNDATION_EXPORT double IGZLocationVersionNumber;
+
+//! Project version string for IGZLocation.
+FOUNDATION_EXPORT const unsigned char IGZLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IGZLocation/PublicHeader.h>
+
+
