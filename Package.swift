06f85e9c59113303feb74f6311d7c97c52b28eea
Alex Rupérez <contact@alexruperez.com>
2017-05-03 15:03:58 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "IGZLocation"
-  origin: "com.intelygenz"
+  name: "IGZLocation",
+  dependencies : [],
+  exclude: ["IGZLocationMap", "IGZLocationMapUITests", "IGZLocationTests"]
 )
